82182d13f728ce1258feea937872efc043f59ab9
Tomasz Aniolowski <aniolowski93@gmail.com>
2019-07-31 18:50:37 +0200

    create Institution entity with repository & service + local variable in CategoryService

--- src/main/java/pl/coderslab/charity/category/CategoryService.java
+++ src/main/java/pl/coderslab/charity/category/CategoryService.java
@@ -1,5 +1,6 @@
 package pl.coderslab.charity.category;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -7,4 +8,12 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
 }
---
+++ src/main/java/pl/coderslab/charity/institution/Institution.java
@@ -0,0 +1,47 @@
+package pl.coderslab.charity.institution;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "institutions")
+public class Institution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Institution: " + name + "(Description: " + description + ")";
+    }
+}
